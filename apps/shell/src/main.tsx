07eba98aa8e9cfadcc30061acfef87c0714b5522
JM <Justmarsh88@gmail.com>
2026-09-10 16:32:22 -0400

    Wire KidOS 2026 shell styles

--- apps/shell/src/main.tsx
+++ apps/shell/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './app.css';
 import './brand-2026.css';
+import './features/home/kidos-shell-2026.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
